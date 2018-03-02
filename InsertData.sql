ac38000634f0ce8c4b9ffbd9992e700350a6164c
Kash Kundargi <kashkundargi@gmail.com>
2018-03-02 18:13:37 -0500

    new SQL script

---
+++ InsertData.sql
@@ -0,0 +1,4 @@
+Delete from Keeper Where KName = 'Peter' And Salary = '2500';
+INSERT INTO keeper (kname, salary) VALUES ('heinz', '5400');
+INSERT INTO keeper (kname, salary) VALUES ('gertrud', '2300');
+INSERT INTO keeper (kname, salary) VALUES ('hilde', '50');
